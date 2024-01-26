cdf2d012fe083423d17d7ab850eb1e53d6277748
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-01-26 09:21:31 +0500

    .

--- ads/views.py
+++ ads/views.py
@@ -59,28 +59,60 @@ class AdCreateView(LoginRequiredMixin, CreateView):
         ad.save()
         return redirect(self.success_url)
 
+# class AdUpdateView(LoginRequiredMixin, UpdateView):
+#     template_name = 'ads/ad_form.html'
+#     success_url = reverse_lazy('ads:all')
+#
+#     def get(self, request, pk):
+#         ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+#         form = CreateForm(instance=ad)
+#         ctx = {'form': form}
+#         return render(request, self.template_name, ctx)
+#
+#     def post(self, request, pk=None):
+#         ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+#         form = CreateForm(request.POST, request.FILES or None, instance=ad)
+#
+#         if not form.is_valid():
+#             ctx = {'form': form}
+#             return render(request, self.template_name, ctx)
+#
+#         ad = form.save(commit=False)
+#         ad.save()
+#
+#         return redirect(self.success_url)
 class AdUpdateView(LoginRequiredMixin, UpdateView):
-    template_name = 'ads/ad_form.html'
+    model = Ad  # Replace with your actual model
+    form_class = CreateForm  # Replace with the actual form you are using
+    template_name = 'ads/ed_form.html'
     success_url = reverse_lazy('ads:all')
 
-    def get(self, request, pk):
-        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
-        form = CreateForm(instance=ad)
+    def get(self, request, pk=None, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
         ctx = {'form': form}
-        return render(request, self.template_name, ctx)
+        return self.render_to_response(ctx)
 
-    def post(self, request, pk=None):
-        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
-        form = CreateForm(request.POST, request.FILES or None, instance=ad)
+    def post(self, request, pk=None, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
 
-        if not form.is_valid():
-            ctx = {'form': form}
-            return render(request, self.template_name, ctx)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
+    def form_valid(self, form):
+        # Add owner to the model before saving
         ad = form.save(commit=False)
+        ad.owner = self.request.user
         ad.save()
-
         return redirect(self.success_url)
+
+    def form_invalid(self, form):
+        ctx = {'form': form}
+        return self.render_to_response(ctx)
+
 
 
 class AdDeleteView(OwnerDeleteView):
@@ -121,8 +153,8 @@ from django.db.utils import IntegrityError
 
 @method_decorator(csrf_exempt, name='dispatch')
 class AddFavoriteView(LoginRequiredMixin, View):
-    def post(self, request, pk) :
-        print("Add PK",pk)
+    def post(self, request, pk):
+        print("Add PK", pk)
         ad = get_object_or_404(Ad, id=pk)
         fav = Fav(user=request.user, ad=ad)
         try:
