4a0094bdaebff4bd6fb04444b0c103b241d48a43
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-01-26 14:19:26 +0500

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ APP_NAME = 'YevhenList'   # Add
 SECRET_KEY = '**************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
