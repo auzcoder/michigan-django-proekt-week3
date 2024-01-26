e60ecbf23401ae06ef7daf6a441586e0f9503cdb
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-01-26 15:33:35 +0500

    .

--- ads/views.py
+++ ads/views.py
@@ -4,26 +4,24 @@ from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from django.core.files.uploadedfile import InMemoryUploadedFile
+from ads.owner import OwnerListView, OwnerDetailView, OwnerDeleteView
 
-from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
-
-from django.views import generic
 from django.urls import reverse
 
 from ads.models import Ad, Comment, Fav
 from ads.forms import CreateForm
 from ads.forms import CommentForm
-
+from django.utils.html import escape
 from django.db.models import Q
+
 
 class AdListView(OwnerListView):
     model = Ad
     template_name = "ads/ad_list.html"
 
-    def get(self, request) :
+    def get(self, request):
         strval = request.GET.get("search", False)
-        if strval :
+        if strval:
             # Simple title-only search
             # objects = Post.objects.filter(title__contains=strval).select_related().order_by('-updated_at')[:10]
 
@@ -34,11 +32,9 @@ class AdListView(OwnerListView):
 
             # post_list = Post.objects.filter(query).select_related().order_by('-updated_at')[:10]
             ad_list = Ad.objects.filter(query)
-        else :
+        else:
             # post_list = Post.objects.all().order_by('-updated_at')[:10]
             ad_list = Ad.objects.all()
-
-
 
         # ctx = {'post_list' : post_list, 'search': strval}
         # return render(request, self.template_name, ctx)
@@ -48,50 +44,72 @@ class AdListView(OwnerListView):
             # rows = [{'id': 2}, {'id': 4} ... ]  (A list of rows)
             rows = request.user.favorite_ads.values('id')
             # favorites = [2, 4, ...] using list comprehension
-            favorites = [ row['id'] for row in rows ]
-        ctx = {'ad_list' : ad_list, 'favorites': favorites, 'search': strval}
+            favorites = [row['id'] for row in rows]
+        ctx = {'ad_list': ad_list, 'favorites': favorites, 'search': strval}
         return render(request, self.template_name, ctx)
+
 
 class AdDetailView(OwnerDetailView):
     model = Ad
     template_name = "ads/ad_detail.html"
-    def get(self, request, pk) :
+
+    # def get(self, request, pk):
+    #     x = Ad.objects.get(id=pk)
+    #     comments = Comment.objects.filter(ad=x).order_by('-updated_at')
+    #     comment_form = CommentForm()
+    #     context = {'ad': x, 'comments': comments, 'comment_form': comment_form}
+    #     return render(request, self.template_name, context)
+    def get(self, request, pk):
         x = Ad.objects.get(id=pk)
         comments = Comment.objects.filter(ad=x).order_by('-updated_at')
         comment_form = CommentForm()
-        context = { 'ad' : x, 'comments': comments, 'comment_form': comment_form }
+        context = {'ad': x, 'comments': comments, 'comment_form': comment_form}
         return render(request, self.template_name, context)
+
 
 class AdCreateView(LoginRequiredMixin, View):
     template_name = 'ads/ad_form.html'
     success_url = reverse_lazy('ads:all')
-    def get(self, request, pk=None) :
+
+    def get(self, request, pk=None):
         form = CreateForm()
-        ctx = { 'form': form }
+        ctx = {'form': form}
         return render(request, self.template_name, ctx)
 
-    def post(self, request, pk=None) :
+    # def post(self, request, pk=None):
+    #     form = CreateForm(request.POST, request.FILES or None)
+    #
+    #     if not form.is_valid():
+    #         ctx = {'form': form}
+    #         return render(request, self.template_name, ctx)
+    #
+    #     # Add owner to the model before saving
+    #     ad = form.save(commit=False)
+    #     ad.owner = self.request.user
+    #     ad.save()
+    #     return redirect(self.success_url)
+    def post(self, request, pk=None):
         form = CreateForm(request.POST, request.FILES or None)
 
-        if not form.is_valid() :
-            ctx = {'form' : form}
+        if form.is_valid():
+            ad = form.save(commit=False)
+            ad.owner = self.request.user
+            ad.save()
+            return redirect(self.success_url)
+        else:
+            ctx = {'form': form}
             return render(request, self.template_name, ctx)
-
-        # Add owner to the model before saving
-        ad = form.save(commit=False)
-        ad.owner = self.request.user
-        ad.save()
-        return redirect(self.success_url)
 
 
 class AdUpdateView(LoginRequiredMixin, View):
     model = Ad
     template_name = "ads/ad_form.html"
     success_url = reverse_lazy('ads:all')
-    def get(self, request, pk) :
+
+    def get(self, request, pk):
         ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
         form = CreateForm(instance=ad)
-        ctx = { 'form': form }
+        ctx = {'form': form}
         return render(request, self.template_name, ctx)
 
     def post(self, request, pk=None) :
@@ -99,7 +117,7 @@ class AdUpdateView(LoginRequiredMixin, View):
         form = CreateForm(request.POST, request.FILES or None, instance=ad)
 
         if not form.is_valid() :
-            ctx = {'form' : form}
+            ctx = {'form': form}
             return render(request, self.template_name, ctx)
 
         ad = form.save(commit=False)
@@ -107,11 +125,13 @@ class AdUpdateView(LoginRequiredMixin, View):
 
         return redirect(self.success_url)
 
+
 class AdDeleteView(OwnerDeleteView):
     model = Ad
     template_name = "ads/ad_confirm_delete.html"
 
-def stream_file(request, pk) :
+
+def stream_file(request, pk):
     ad = get_object_or_404(Ad, id=pk)
     response = HttpResponse()
     response['Content-Type'] = ad.content_type
@@ -119,12 +139,14 @@ def stream_file(request, pk) :
     response.write(ad.picture)
     return response
 
+
 class CommentCreateView(LoginRequiredMixin, View):
     def post(self, request, pk) :
         f = get_object_or_404(Ad, id=pk)
         comment = Comment(text=request.POST['comment'], owner=request.user, ad=f)
         comment.save()
         return redirect(reverse('ads:ad_detail', args=[pk]))
+
 
 class CommentDeleteView(OwnerDeleteView):
     model = Comment
@@ -142,10 +164,11 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.db.utils import IntegrityError
 
+
 @method_decorator(csrf_exempt, name='dispatch')
 class AddFavoriteView(LoginRequiredMixin, View):
-    def post(self, request, pk) :
-        print("Add PK",pk)
+    def post(self, request, pk):
+        print("Add PK", pk)
         t = get_object_or_404(Ad, id=pk)
         fav = Fav(user=request.user, ad=t)
         try:
@@ -154,9 +177,10 @@ class AddFavoriteView(LoginRequiredMixin, View):
             pass
         return HttpResponse()
 
+
 @method_decorator(csrf_exempt, name='dispatch')
 class DeleteFavoriteView(LoginRequiredMixin, View):
-    def post(self, request, pk) :
+    def post(self, request, pk):
         print("Delete PK",pk)
         t = get_object_or_404(Ad, id=pk)
         try:
